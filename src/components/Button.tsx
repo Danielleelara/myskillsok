1d2c305777177d7c6702d7ec2a21e20f990c78e3
danielleelara <dany.sa1@hotmail.com>
2022-08-14 21:06:04 -0300

    feat: instalando Typescript

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {TouchableOpacity, Text, StyleSheet} from 'react-native';
 
-export function ButtonComponent({onPress, children}) {
+
+export function ButtonComponent({onPress}) {
   return (
     <TouchableOpacity
       style={styles.button}
